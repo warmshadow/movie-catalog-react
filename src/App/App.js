5e4f20e97d5105188e667bbe14ecfcf81e67e3e7
karolis <laukyskarolis@gmail.com>
2020-08-13 22:55:39 +0300

    connected App to the store

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Container from 'react-bootstrap/Container';
+import { setConfig as setConfigAction } from './actions';
 import NavigationBar from './nav/NavigationBar';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import './App.css';
 
-function App() {
+function App({ config, setConfig }) {
+  useEffect(() => {
+    setConfig();
+  }, [setConfig]);
+
+  console.log(config);
   return (
     <Container className="App">
       <BrowserRouter>
@@ -22,4 +29,16 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    config: state.config,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setConfig: () => dispatch(setConfigAction()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
+import store from './App/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
