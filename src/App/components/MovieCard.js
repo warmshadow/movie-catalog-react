519943e00725fc1fb13bf246d55072037470ffee
karolis <laukyskarolis@gmail.com>
2020-08-14 13:47:02 +0300

    wrapped moviecard in a router link

--- src/App/components/MovieCard.js
+++ src/App/components/MovieCard.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function MovieCard({ movie, baseUrl }) {
   return (
-    <Card>
-      <Row noGutters style={{ width: '100%' }}>
-        <Col md={2}>
-          <Card.Img src={`${baseUrl}w342${movie.poster_path}`} />
-        </Col>
-        <Col md={10} className="d-flex align-items-center">
-          <Card.Body>
-            <Card.Title>{movie.title}</Card.Title>
-            <Card.Subtitle>{movie.release_date}</Card.Subtitle>
-          </Card.Body>
-        </Col>
-      </Row>
-    </Card>
+    <Link to={`/movie/${movie.id}`}>
+      <Card>
+        <Row noGutters style={{ width: '100%' }}>
+          <Col md={2}>
+            <Card.Img src={`${baseUrl}w342${movie.poster_path}`} />
+          </Col>
+          <Col md={10} className="d-flex align-items-center">
+            <Card.Body>
+              <Card.Title>{movie.title}</Card.Title>
+              <Card.Subtitle>{movie.release_date}</Card.Subtitle>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </Link>
   );
 }
 
