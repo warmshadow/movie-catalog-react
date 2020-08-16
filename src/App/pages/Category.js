a0632aab9e146b914115d5c0b86ef81ce4e5f40a
karolis <laukyskarolis@gmail.com>
2020-08-16 16:45:15 +0300

    category component with initial categories

--- src/App/pages/Category.js
+++ src/App/pages/Category.js
@@ -1,7 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from 'react-bootstrap/Spinner';
+import { setMoviesCategory as setMoviesCategoryAction } from '../actions';
+import MovieList from '../components/MovieList';
+import pageIsInt from '../helpers';
 
-function Category() {
-  return <div></div>;
+const CATEGORIES = {
+  toprated: {
+    route: 'toprated',
+    name: 'Top Rated',
+    params: {
+      sort_by: 'vote_average.desc',
+      'vote_count.gte': 200,
+    },
+  },
+  popular: {
+    name: 'Popular',
+    params: {
+      sort_by: 'popularity.desc',
+    },
+  },
+  documentary: {
+    name: 'Documentary',
+    params: {
+      with_genres: '99',
+      sort_by: 'vote_average.desc',
+      'vote_count.gte': 200,
+    },
+  },
+  lithuanian: {
+    name: 'Lithuanian',
+    params: {
+      with_original_language: 'lt',
+    },
+  },
+};
+
+function Category({ movies, baseUrl, setMoviesCategory }) {
+  const { title, pageNum } = useParams();
+  const basePath = `/category/${title}`;
+
+  useEffect(() => {
+    if (CATEGORIES[title]) {
+      const { params } = CATEGORIES[title];
+      setMoviesCategory(params, pageNum);
+    }
+  }, [title, pageNum, setMoviesCategory]);
+
+  if (!CATEGORIES[title]) return <Redirect to="/notfound" />;
+
+  if (!pageIsInt(pageNum)) return <Redirect to="/notfound" />;
+
+  if (movies.isPending) return <Spinner animation="border" />;
+
+  return (
+    <>
+      <h2>Movies for category:</h2>
+      <h3>{CATEGORIES[title].name}</h3>
+      <MovieList movies={movies} baseUrl={baseUrl} basePath={basePath} />
+    </>
+  );
 }
 
-export default Category;
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+    baseUrl: state.config.images.secure_base_url,
+  };
+};
+
+const mapDispatchToProps = {
+  setMoviesCategory: setMoviesCategoryAction,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Category);
