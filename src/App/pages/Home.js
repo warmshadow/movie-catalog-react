0022667fcc2a030e5479a1c2ff7ca2520941687a
karolis <laukyskarolis@gmail.com>
2020-08-16 16:52:51 +0300

    homepage. render list of links to categories

--- src/App/pages/Home.js
+++ src/App/pages/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <ListGroup>
+        <ListGroup.Item>
+          <Link to="/category/toprated">Top Rated</Link>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Link to="/category/popular">Popular</Link>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Link to="/category/documentary">Documentary</Link>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Link to="/category/lithuanian">Lithuanian</Link>
+        </ListGroup.Item>
+      </ListGroup>
     </div>
   );
 }
