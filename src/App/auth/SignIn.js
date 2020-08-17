2d9d1a94f728bf5380e6f112fb18e9d749975fbb
karolis <laukyskarolis@gmail.com>
2020-08-17 19:35:57 +0300

    modified SignIn/SignUp components

--- src/App/auth/SignIn.js
+++ src/App/auth/SignIn.js
@@ -17,34 +17,34 @@ function SignIn({ auth, authError, signIn }) {
   if (auth.uid) return <Redirect to="/" />;
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="email">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          value={email}
-          placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Form.Text className="text-muted">
-          We will never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <>
+      <h1>Sign In</h1>
+      <Form onSubmit={handleSubmit} className="mt-3">
+        <Form.Group controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Sign In
-      </Button>
-      {authError && <p>{authError}</p>}
-    </Form>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign In
+        </Button>
+        {authError && <p>{authError}</p>}
+      </Form>
+    </>
   );
 }
 
--- src/App/auth/SignUp.js
+++ src/App/auth/SignUp.js
@@ -19,52 +19,52 @@ function SignUp({ auth, authError, signUp }) {
   if (auth.uid) return <Redirect to="/" />;
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="email">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          value={email}
-          placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Form.Text className="text-muted">
-          We will never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <>
+      <h1>Sign Up</h1>
+      <Form onSubmit={handleSubmit} className="mt-3">
+        <Form.Group controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="firstName">
-        <Form.Label>First name</Form.Label>
-        <Form.Control
-          value={firstName}
-          placeholder="First name"
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-      </Form.Group>
+        <Form.Group controlId="firstName">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            value={firstName}
+            placeholder="First name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="lastName">
-        <Form.Label>Last name</Form.Label>
-        <Form.Control
-          value={lastName}
-          placeholder="Last name"
-          onChange={(e) => setLastName(e.target.value)}
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Sign In
-      </Button>
-      {authError && <p>{authError}</p>}
-    </Form>
+        <Form.Group controlId="lastName">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            value={lastName}
+            placeholder="Last name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign In
+        </Button>
+        {authError && <p>{authError}</p>}
+      </Form>
+    </>
   );
 }
 
