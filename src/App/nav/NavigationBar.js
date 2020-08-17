4d4fe0c12d789636174685ef41b91960cfb53f0a
karolis <laukyskarolis@gmail.com>
2020-08-17 08:40:44 +0300

    rendering different nav links based on auth state

--- src/App/nav/NavigationBar.js
+++ src/App/nav/NavigationBar.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 
-function NavigationBar() {
+function NavigationBar({ auth }) {
   return (
     <Navbar bg="dark" variant="dark" fixed="top">
       <Link to="/">
         <Navbar.Brand>Movie Catalog</Navbar.Brand>
       </Link>
       <SearchBar />
-      {/* <SignedInLinks /> */}
-      <SignedOutLinks />
+      {auth.uid ? <SignedInLinks /> : <SignedOutLinks />}
     </Navbar>
   );
 }
 
-export default NavigationBar;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
+
+export default connect(mapStateToProps)(NavigationBar);
--- src/App/nav/SignedOutLinks.js
+++ src/App/nav/SignedOutLinks.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 function SignedOutLinks() {
   return (
     <Nav className="ml-auto">
-      <NavLink to="/signin">Sign In</NavLink>
-      <NavLink to="/signup">Sign Up</NavLink>
+      <NavLink to="/signin">
+        <Button variant="link">Sign In</Button>
+      </NavLink>
+      <NavLink to="/signup">
+        <Button variant="link">Sign Up</Button>
+      </NavLink>
     </Nav>
   );
 }
