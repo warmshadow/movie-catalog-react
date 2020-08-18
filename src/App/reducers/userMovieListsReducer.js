6f47d13e71aefd29a9463327595a1e483249e2c5
karolis <laukyskarolis@gmail.com>
2020-08-18 06:10:10 +0300

    temporary alerts for now

--- src/App/reducers/userMovieListsReducer.js
+++ src/App/reducers/userMovieListsReducer.js
@@ -1,13 +1,15 @@
 const userMovieListsReducer = (state = { isPending: true }, action) => {
   switch (action.type) {
     case 'CREATE_MOVIE_LIST_SUCCESS':
-      console.log('successfully created movie list');
+      // temporary alert
+      alert('successfully created movie list');
       return state;
     case 'CREATE_MOVIE_LIST_ERROR':
       console.log('error in creating movie list');
       return state;
     case 'ADD_MOVIE_SUCCESS':
-      console.log('successfully added movie to list');
+      // temporary alert
+      alert('successfully added movie to list');
       return state;
     case 'ADD_MOVIE_ERROR':
       console.log('error adding movie to list');
