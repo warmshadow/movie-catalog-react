b4a3e98ea3b97a30e1a842ab49fc25d98c1b6219
karolis <laukyskarolis@gmail.com>
2020-08-25 00:31:11 +0300

    connected remove logic to list and moviecard

--- src/App/components/MovieCard.js
+++ src/App/components/MovieCard.js
@@ -3,24 +3,44 @@ import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
-function MovieCard({ id, posterPath, title, releaseDate, baseUrl }) {
+const Options = ({ remove }) => {
+  if (!remove) return null;
+
   return (
-    <Link to={`/movie/${id}`} className="text-decoration-none">
-      <Card bg="secondary" className="text-dark mb-4 moviecard">
-        <Row noGutters style={{ width: '100%' }}>
-          <Col md={2}>
-            <Card.Img src={`${baseUrl}w342${posterPath}`} />
-          </Col>
-          <Col md={10} className="d-flex align-items-center">
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Subtitle>{releaseDate}</Card.Subtitle>
-            </Card.Body>
-          </Col>
-        </Row>
-      </Card>
-    </Link>
+    <Card.Body
+      className="d-flex align-items-center justify-content-center"
+      style={{
+        position: 'absolute',
+        right: 0,
+        zIndex: 100,
+      }}
+    >
+      <Button variant="outline-dark" onClick={remove}>
+        Remove
+      </Button>
+    </Card.Body>
+  );
+};
+
+function MovieCard({ id, posterPath, title, releaseDate, baseUrl, remove }) {
+  return (
+    <Card bg="secondary" className="text-dark mb-4 moviecard">
+      <Row noGutters style={{ width: '100%' }}>
+        <Link to={`/movie/${id}`} className="text-decoration-none stretched-link" />
+        <Col md={2}>
+          <Card.Img src={`${baseUrl}w342${posterPath}`} />
+        </Col>
+        <Col md={10} className="d-flex align-items-center">
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Subtitle>{releaseDate}</Card.Subtitle>
+          </Card.Body>
+          <Options remove={remove} />
+        </Col>
+      </Row>
+    </Card>
   );
 }
 
--- src/App/pages/List.js
+++ src/App/pages/List.js
@@ -4,9 +4,10 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 import Spinner from 'react-bootstrap/Spinner';
+import { removeMovieFromList as removeMovieFromListAction } from '../actions';
 import MovieCard from '../components/MovieCard';
 
-function List({ auth, userMovieList, requesting, baseUrl }) {
+function List({ auth, userMovieList, requesting, baseUrl, listId, removeMovieFromList }) {
   if (!auth.uid) return <Redirect to="/signin" />;
 
   if (requesting === false) {
@@ -21,14 +22,17 @@ function List({ auth, userMovieList, requesting, baseUrl }) {
             userMovieList.movies.map((movie) => {
               const { id, posterPath, title, releaseDate } = movie;
               return (
-                <MovieCard
-                  id={id}
-                  posterPath={posterPath}
-                  title={title}
-                  releaseDate={releaseDate}
-                  baseUrl={baseUrl}
-                  key={id}
-                />
+                <>
+                  <MovieCard
+                    id={id}
+                    posterPath={posterPath}
+                    title={title}
+                    releaseDate={releaseDate}
+                    baseUrl={baseUrl}
+                    key={id}
+                    remove={() => removeMovieFromList(listId, movie)}
+                  />
+                </>
               );
             })
           ) : (
@@ -55,11 +59,16 @@ const mapStateToProps = (state, ownProps) => {
     userMovieList,
     requesting,
     baseUrl: state.config.images.secure_base_url,
+    listId: id,
   };
+};
+
+const mapDispatchToProps = {
+  removeMovieFromList: removeMovieFromListAction,
 };
 
 export default compose(
   withRouter,
-  connect(mapStateToProps),
+  connect(mapStateToProps, mapDispatchToProps),
   firestoreConnect([{ collection: 'userMovieLists' }])
 )(List);
