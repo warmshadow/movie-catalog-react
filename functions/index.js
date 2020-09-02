c355b941e8561f99d6378b978ae0c350d229f013
karolis <laukyskarolis@gmail.com>
2020-09-02 19:07:01 +0300

    cloud function to create initial lists for new user

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const createRatingsList = (user, list) => {
+  return admin.firestore().collection('ratingsLists').doc(user.uid).set(list);
+};
+
+const createWatchlist = (user, list) => {
+  return admin.firestore().collection('watchlists').doc(user.uid).set(list);
+};
+
+const createInitialLists = (user, list) => {
+  return Promise.all([createRatingsList(user, list), createWatchlist(user, list)]).then(() =>
+    console.log('Created initial lists for user')
+  );
+};
+
+exports.userJoined = functions.auth.user().onCreate((user) => {
+  const list = { createdAt: admin.firestore.FieldValue.serverTimestamp(), items: [] };
+
+  return createInitialLists(user, list);
+});
