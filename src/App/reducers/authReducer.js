1327e0b79859d81e7fa6a84c8ef71a650438ee50
karolis <laukyskarolis@gmail.com>
2020-09-08 00:51:28 +0300

    refactored auth reducer

--- src/App/reducers/authReducer.js
+++ src/App/reducers/authReducer.js
@@ -5,23 +5,12 @@ const initState = {
 const authReducer = (state = initState, action) => {
   switch (action.type) {
     case 'SIGNIN_ERROR':
-      console.log('signin error');
+    case 'SIGNOUT_ERROR':
+    case 'SIGNUP_ERROR':
       return { ...state, authError: action.err.message };
     case 'SIGNIN_SUCCESS':
-      console.log('signin success');
-      return { ...state, authError: null };
     case 'SIGNOUT_SUCCESS':
-      console.log('signOut success');
-      return state;
-    case 'SIGNOUT_ERROR':
-      console.log('signOut error');
-      return { ...state, authError: action.err.message };
     case 'SIGNUP_SUCCESS':
-      console.log('signUp success');
-      return { ...state, authError: null };
-    case 'SIGNUP_ERROR':
-      console.log('signUp error');
-      return { ...state, authError: action.err.message };
     case 'CLEAR_AUTH_ERROR':
       return { ...state, authError: null };
     default:
