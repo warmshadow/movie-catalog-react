06f929160e7d1dfbee5f64f379f5abf29853eb61
karolis <laukyskarolis@gmail.com>
2020-09-08 23:53:55 +0300

    inline style to bs classes

--- src/App/components/MovieDetails.js
+++ src/App/components/MovieDetails.js
@@ -19,68 +19,58 @@ function MovieDetails({ movie, directors, baseUrl, imdbBaseUrl, addToList, addTo
   } = movie;
 
   return (
-    <>
-      <Row noGutters style={{ width: '100%', display: 'flex', flexWrap: 'wrap' }}>
-        <Col md={6} lg={4} style={{ display: 'flex' }}>
-          <Card.Img
-            src={`${baseUrl}w780${posterPath}`}
-            alt="poster"
-            style={{ maxHeight: '100%' }}
-          />
-        </Col>
-        <Col md={6} lg={8} style={{ display: 'flex' }}>
-          <Card className="bg-transparent border-0">
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Text className="font-italic" key={tagline}>
-                {tagline}
+    <Row noGutters>
+      <Col md={6} lg={4} className="d-flex">
+        <Card.Img src={`${baseUrl}w780${posterPath}`} alt="poster" />
+      </Col>
+      <Col md={6} lg={8} className="d-flex">
+        <Card className="bg-transparent border-0">
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text className="font-italic" key={tagline}>
+              {tagline}
+            </Card.Text>
+            {directors.map((director) => (
+              <Card.Text key={director.name}>{`Dir. ${director.name}`}</Card.Text>
+            ))}
+            <Card.Text key={voteAverage}>
+              {`${voteAverage} / ${runtime}min / ${originalLanguage}`}
+            </Card.Text>
+            {genres.map((genre) => (
+              <Card.Text className="d-inline" key={genre.id}>
+                {`${genre.name}  `}
               </Card.Text>
-              {directors.map((director) => (
-                <Card.Text key={director.name}>{`Dir. ${director.name}`}</Card.Text>
-              ))}
-              <Card.Text key={voteAverage}>
-                {`${voteAverage} / ${runtime}min / ${originalLanguage}`}
-              </Card.Text>
-              {genres.map((genre) => (
-                <Card.Text className="d-inline" key={genre.id}>
-                  {`${genre.name}  `}
-                </Card.Text>
-              ))}
-              <Card.Subtitle className="text-muted font-italic my-3" key={overview.substring(0, 9)}>
-                {overview}
-              </Card.Subtitle>
-              {imdbId && (
-                <Button
-                  key={imdbId}
-                  variant="warning"
-                  href={`${imdbBaseUrl}${imdbId}`}
-                  target="_blank"
-                >
-                  IMDB
-                </Button>
-              )}
-              {homepage && (
-                <Button key={homepage} variant="info" href={homepage} target="_blank">
-                  Homepage
-                </Button>
-              )}
-            </Card.Body>
-            <Card.Footer>
+            ))}
+            <Card.Subtitle className="text-muted font-italic my-3" key={overview.substring(0, 9)}>
+              {overview}
+            </Card.Subtitle>
+            {imdbId && (
               <Button
-                style={{ marginRight: '1rem' }}
-                variant="outline-dark"
-                onClick={() => addToList(movie)}
+                key={imdbId}
+                variant="warning"
+                href={`${imdbBaseUrl}${imdbId}`}
+                target="_blank"
               >
-                Add to List
+                IMDB
               </Button>
-              <Button variant="outline-dark" onClick={() => addToWatchlist(movie)}>
-                Add to Watchlist
+            )}
+            {homepage && (
+              <Button key={homepage} variant="info" href={homepage} target="_blank">
+                Homepage
               </Button>
-            </Card.Footer>
-          </Card>
-        </Col>
-      </Row>
-    </>
+            )}
+          </Card.Body>
+          <Card.Footer>
+            <Button className="mr-3" variant="outline-dark" onClick={() => addToList(movie)}>
+              Add to List
+            </Button>
+            <Button variant="outline-dark" onClick={() => addToWatchlist(movie)}>
+              Add to Watchlist
+            </Button>
+          </Card.Footer>
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
