8597ed97798c9deae016e67fe73c17b32f8901b4
karolis <laukyskarolis@gmail.com>
2020-09-14 14:21:56 +0300

    logging to console instead of alert

--- src/App/reducers/ratingsReducer.js
+++ src/App/reducers/ratingsReducer.js
@@ -1,15 +1,13 @@
 const ratingsReducer = (state = { isPending: true }, action) => {
   switch (action.type) {
     case 'SET_RATING_SUCCESS':
-      // temporary alert
-      alert('successfully set a movie rating');
+      console.log('successfully set a movie rating');
       return state;
     case 'SET_RATING_ERROR':
       console.log('error setting movie rating', action.payload);
       return state;
     case 'REMOVE_RATING_SUCCESS':
-      // temporary alert
-      alert('successfully removed a movie rating');
+      console.log('successfully removed a movie rating');
       return state;
     case 'REMOVE_RATING_ERROR':
       console.log('error removing movie rating', action.payload);
--- src/App/reducers/watchlistReducer.js
+++ src/App/reducers/watchlistReducer.js
@@ -1,15 +1,13 @@
 const watchlistReducer = (state = { isPending: true }, action) => {
   switch (action.type) {
     case 'ADD_MOVIE_WATCHLIST_SUCCESS':
-      // temporary alert
-      alert('successfully added movie to watchlist');
+      console.log('successfully added movie to watchlist');
       return state;
     case 'ADD_MOVIE_WATCHLIST_ERROR':
       console.log('error adding movie to watchlist', action.payload);
       return state;
     case 'REMOVE_MOVIE_WATCHLIST_SUCCESS':
-      // temporary alert
-      alert('successfully removed movie from watchlist');
+      console.log('successfully removed movie from watchlist');
       return state;
     case 'REMOVE_MOVIE_WATCHLIST_ERROR':
       console.log('error removing movie from watchlist', action.payload);
